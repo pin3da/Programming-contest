1bf292a2f1c22915f00264a05c964876174b39d1
Manuel <manuel.felipe.pineda@gmail.com>
2012-03-27 00:34:19 -0500

    ya dio :)

--- UVa/10242-Fourth/10242.c
+++ UVa/10242-Fourth/10242.c
@@ -1,38 +1,35 @@
 /*	Manuel Pineda.
-	No accepted yet.
 	four point in a paralelogram.
 				  2
     			 /
 				/		
 		1-----3/
 	  4 = 2 -(3 - 1);
-	the point 3 is the repeated point
+	the point 3 is the repeated point                     
 */
 #include<stdio.h>
 int main(){
-	int i,j;
-	double p[8];
-	double r1x,r2x,r3x,r1y,r2y,r3y,a1,a2;
-	while(scanf("%lf %lf %lf %lf %lf %lf %lf %lf\n",p,p+1,p+2,p+3,p+4,p+5,p+6,p+7)==8){
-		for(i=0;i<8;i+=2)
-			for(j=i+2;j<8;j+=2)
-				if(p[i]==p[j]){
-					r3x=p[i];
-					r3y=p[i+1];
-					break;
-				}
-		 for(i=0;i<8;i+=2)
-			if(p[i]!=r3x){
-				r2x=p[i];
-				r2y=p[i+1];
-			}
-		 for(i=6;i>=0;i-=2)
-			if(p[i]!=r3x){
-				r1x=p[i];
-				r1y=p[i+1];
-			}
-		a1=r2x-(r3x-r1x);
-		a2=r2y-(r3y-r1y);
+	double x1,x2,x3,x4,y1,y2,y3,y4,a1,a2;
+	while(scanf("%lf %lf %lf %lf %lf %lf %lf %lf",&x1,&y1,&x2,&y2,&x3,&y3,&x4,&y4)==8){
+		if(x1==x2 && y1==y2){
+			a1=x4-(x1-x3);
+			a2=y4-(y1-y3);
+		}else if(x1==x3 && y1==y3){
+			a1=x2-(x1-x4);
+			a2=y2-(y1-y4);
+		}else if(x1==x4 && y1==y4){
+			a1=x3-(x1-x2);
+			a2=y3-(y1-y2);
+		}else if(x2==x3 && y2==y3){			
+			a1=x1-(x2-x4);
+			a2=y1-(y2-y4);
+		}else if(x2==x4 && y2==y4){			
+			a1=x3-(x2-x1);
+			a2=y3-(y2-y1);
+		}else if(x3==x4 && x3==x4){			
+			a1=x1-(x3-x2);
+			a2=y1-(y3-y2);
+		}
 		printf("%.3lf %.3lf\n", a1,a2);
 	}
 	return 0;
