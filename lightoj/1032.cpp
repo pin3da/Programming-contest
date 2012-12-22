4e1993bc5f416f401714c93ff3652a317ecf247a
pin3da <manuel.felipe.pineda@gmail.com>
2012-12-21 19:51:42 -0500

    DP problems

---
+++ lightoj/1032.cpp
@@ -0,0 +1,33 @@
+using namespace std;
+#include<algorithm>
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+#include<map>
+
+#include<climits>
+#include<cstring>
+#include<cstdio>
+#include<cmath>
+
+#define For(i,a) for(int i=0;i<a;++i)
+#define foreach(x,v) for(typeof (v).begin() x = (v).begin(); x!= (v).end(); x++)
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define D(x) cout<< #x " = "<<(x)<<endl
+#define Dbg if(1)
+#define MAXNODES 1000
+#define MAX(a,b) ((a>b)?(a):(b))
+
+int suma[4294967296];
+
+int main(){
+	int numcas;cin>>numcas;
+	for(int cid=1;cid<=numcas;++cid){
+        cout<<"Case "<<cid<<": "<<endl;   
+	}
+	return 0;
+}
