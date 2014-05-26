52d50427567f2efcd8adae1f15f18c20df16ef68
pin3da <manuel.felipe.pineda@gmail.com>
2014-05-26 00:31:18 -0500

    Solved: D problem - round 247, Codeforces.

--- solved/CodeForces/round247/D.cpp
+++ solved/CodeForces/round247/D.cpp
@@ -1,5 +1,3 @@
-// Por terminar !!
-
 using namespace std;
 #include<bits/stdc++.h>
 #define foreach(it, x) for (typeof (x).begin() it = (x).begin(); it != (x).end(); ++it)
@@ -10,7 +8,7 @@ using namespace std;
 
 const int MN = 10001;
 
-int m,k;
+long long m,k;
 string line;
 
 long long memo[66][66][2];
@@ -19,23 +17,22 @@ const long long inf = 1e18;
 
 long long dp(int index, int cuenta, int tope) {
   if (index == line.size()) return cuenta == k;
-
   if (memo[index][cuenta][tope] != -1) return memo[index][cuenta][tope];
-
-  long long ans = dp (index + 1, cuenta, tope);
-  if (!tope) {
-    if (index == 0) tope = line[0] == '1';
-    ans = ans + dp(index +1 , cuenta + 1, tope and line[index] == '1');
+  long long ans = 0;
+  if (tope) {
+    if (line[index] == '1')
+      ans = dp(index + 1, cuenta + 1, 1) + dp(index + 1, cuenta, 0);
+    else
+      ans = dp(index + 1, cuenta, 1) ;
   }
-
+   else
+    ans = dp(index + 1, cuenta + 1, 0) + dp(index + 1, cuenta, 0) ;
   return memo[index][cuenta][tope] = ans;
 }
 
 string fun( long long n) {
   string ans;
-
   while(n) {
-
     if (n & 1LL)
       ans = '1' + ans;
     else
@@ -46,18 +43,14 @@ string fun( long long n) {
 }
 
 long long cuenta(long long n) {
-
   long long a = n;
   memset(memo, -1, sizeof memo);
   line = fun (a);
-  long long aa = dp(0,0,0);
-
-  long long b = n<<1;
-
+  long long aa = dp(0,0,1);
+  long long b =  n << 1;
   memset(memo, -1, sizeof memo);
   line = fun (b);
-  long long bb = dp(0,0,0);
-
+  long long bb = dp(0,0,1);
   return bb - aa;
 }
 
@@ -65,37 +58,21 @@ int main() { ___
 
   cin>>m>>k;
 
-  /*if (m == 1 and k == 1){
-    cout<<1<<endl;
-    return 0;
-  }
-  long long lo = 0;
+  long long lo = 1;
   long long hi = inf;
 
-  while (lo < hi) {
+  while (lo <= hi) {
 
     long long mid  = (lo + hi)>>1;
-
-    if (lo + 1 == hi) {
-      lo = hi;
-      break;
-    }
-
-    if (cuenta(mid) < m) {
-      lo = mid + 1;
-    } else {
-      hi = mid;
-    }
-  }
-
-  cout<<((lo + hi)>>1)<<endl;*/
-
-  for (long long i = 1; i <= inf; ++i) {
-    D(cuenta(i));
-    if (cuenta(i) == m){
-      cout<<i<<endl;
+    long long cmid = cuenta(mid);
+    if (cmid == m) {
+      cout<<mid<<endl;
       return 0;
     }
+    if (cuenta(mid) < m)
+      lo = mid + 1;
+    else
+      hi = mid - 1;
   }
 
   return 0;
