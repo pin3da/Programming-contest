cbc9ad47d5cc09db7e477f17c3ac254efabe15dd
pin3da <manuel.felipe.pineda@gmail.com>
2014-06-30 15:42:27 -0500

    Suffix array code

---
+++ codes/strings/suffix_array.cpp
@@ -0,0 +1,58 @@
+/**
+ * O (n log^2 (n))
+ * See http://web.stanford.edu/class/cs97si/suffix-array.pdf for reference
+ * */
+using namespace std;
+#include<bits/stdc++.h>
+#define D(x) cout<<#x " = "<<(x)<<endl
+
+struct entry{
+  int a, b, p;
+  entry(){}
+  entry(int x, int y, int z): a(x), b(y), p(z){}
+  bool operator < (const entry &o) const {
+    return (a == o.a) ? (b < o.b) : (a < o.a);
+  }
+};
+
+struct SuffixArray{
+  const int N;
+  string s;
+  vector<vector<int> > P;
+  vector<entry> M;
+
+  SuffixArray(const string &s) : N(s.length()) , s(s), P(1, vector<int> (N, 0)), M(N) {
+    for (int i = 0; i < N; ++i)
+      P[0][i] = s[i];
+
+    for (int skip = 1, level = 1; skip < N; skip *= 2, level++) {
+      P.push_back(vector<int>(N, 0));
+      for (int i = 0 ; i < N; ++i) {
+        int next = ((i + skip) < N) ? P[level - 1][i + skip] : -10000;
+        M[i] = entry(P[level - 1][i], next, i);
+      }
+      sort(M.begin(), M.end());
+      for (int i = 0; i < N; ++i)
+        P[level][M[i].p] =  (i > 0 and M[i].a == M[i - 1].a and M[i].b == M[i - 1].b) ? P[level - 1][M[i - 1].p] : i;
+    }
+  }
+
+  vector<int> getSuffixArray(){
+    return P.back();
+  }
+
+  // returns the length of the longest common prefix of s[i...L-1] and s[j...L-1]
+  int longestCommonPrefix(int i, int j) {
+    int len = 0;
+    if (i == j) return N - i;
+    for (int k = P.size() - 1; k >= 0 && i < N && j < N; --k) {
+      if (P[k][i] == P[k][j]) {
+        i += 1 << k;
+        j += 1 << k;
+        len += 1 << k;
+      }
+    }
+    return len;
+  }
+};
+
