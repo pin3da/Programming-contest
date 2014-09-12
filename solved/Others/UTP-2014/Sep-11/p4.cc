b01fc496fabedef2a36fb3b1cb46541427ee985a
pin3da <manuel.felipe.pineda@gmail.com>
2014-09-11 23:10:05 -0500

    UTP training 11 Sep

--- solved/Others/UTP-2014/Sep-11/p4.cc
+++ solved/Others/UTP-2014/Sep-11/p4.cc
@@ -4,42 +4,17 @@ using namespace std;
 #define D(x) cout<< #x " = " << (x) <<endl
 #define endl '\n'
 
-const int MN = 100001;
-long long sum[MN];
-long long val[MN];
-
-int lower(int x) {
-  for (int i = 0; i < 30; ++i)
-    if ((x>>i) & 1) return (1<<i);
-}
-
 int main() { ___
 
-  for (int i = 1; i < MN; ++i) {
-    val[i] = lower(i);
-    sum[i] = sum[i - 1] + val[i];
-  }
   int n,k;
   cin>>n>>k;
-
-  if (sum[k] < n) {
-    cout<<-1<<endl;
-    return 0;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin>>a[i];
   }
+  sort(a.begin(), a.end());
+  reverse(a.begin(), a.end());
+  cout<<a[k - 1]<<endl;
 
-  vector<int> ans;
-
-  for (int i = k; i > 0; i--) {
-    if (val[i] > n) continue;
-    n -= val[i];
-    ans.push_back(i);
-  }
-
-  cout<<ans.size()<<endl;
-  for (int i = 0; i < ans.size(); ++i) {
-    if (i) cout<<" ";
-    cout<<ans[i];
-  }
-  cout<<endl;
   return 0;
 }
