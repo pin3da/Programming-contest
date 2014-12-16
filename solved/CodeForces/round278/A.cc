2adfdaca1b0ffeaa5d44ce66726a4a7cadff5f2f
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2014-12-16 14:30:13 -0500

    Codeforces round 278

---
+++ solved/CodeForces/round278/A.cc
@@ -0,0 +1,22 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+bool cool(int n) {
+  n = abs(n);
+  while (n) {
+    if (n % 10 == 8) return true;
+    n /= 10;
+  }
+  return false;
+}
+
+int main() {
+
+  int n;
+  while (cin >> n) {
+    int ans = 1;
+    while (!cool(++n)) ans++;
+    cout << ans << endl;
+  }
+  return 0;
+}
---
+++ solved/CodeForces/round278/C.cc
@@ -0,0 +1,90 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define D(x) cout << #x << " = " << (x) << endl
+
+const int inf = 1 << 20;
+const int MN = 222;
+
+int dp[1001][MN][MN];
+
+int solve(vector<int> &y, vector<int> &m, vector<int> &cost, int h, int a, int d) {
+
+  if (h >= 1001 || a >= MN || d >= MN )
+    return inf;
+
+  if (dp[h][a][d] != -1)
+    return dp[h][a][d];
+
+  int hh = h + y[0];
+  int aa = a + y[1];
+  int dd = d + y[2];
+
+  int damage = max(0, m[1] - dd);
+  int ans = inf;
+  if (damage < hh) {
+    int x = ((damage == 0) ? inf : ((hh - 1) / damage));
+
+    if (x * max(0, aa - m[2]) >= m[0])
+      return dp[h][a][d] = 0;
+
+    ans = min(ans, cost[1] + solve(y, m, cost, h, a + 1, d));
+  }
+  ans = min(ans, cost[0] + solve(y, m, cost, h + 1, a, d));
+  ans = min(ans, cost[2] + solve(y, m, cost, h, a, d + 1));
+  return dp[h][a][d] = ans;
+}
+
+
+/*
+ *bool win(int h, int a, int d, int H, int A, int D) {
+ *  if (h <= 0) return false;
+ *  if (H <= 0) return true;
+ *  int damy = max(0, a - D);
+ *  int damm = max(0, A - d);
+ *
+ *  if (damy == 0)
+ *    return false;
+ *
+ *  int x = (H + damy - 1) / damy;
+ *  if (h - x * damm <= 0)
+ *    return false;
+ *  return true;
+ *}
+ */
+
+int main() {
+  vector<int> a(3), b(3), c(3);
+
+  for (int i = 0; i < 3; ++i)
+    cin >> a[i];
+
+  for (int i = 0; i < 3; ++i)
+    cin >> b[i];
+
+  for (int i = 0; i < 3; ++i)
+    cin >> c[i];
+
+  memset(dp, -1, sizeof dp);
+  cout << solve(a, b, c, 0, 0, 0) << endl;
+
+/*
+ *  int ans = inf;
+ *  for (int i = 0; i < 1001; ++i) {
+ *    for (int j = 0; j < MN; ++j) {
+ *      for (int k = 0; k < MN; ++k) {
+ *        int h  = a[0] + i;
+ *        int at = a[1] + j;
+ *        int de = a[2] + k;
+ *        if (win(h, at, de, b[0], b[1], b[2])) {
+ *          ans = min(ans, i * c[0] + j * c[1] + k * c[2]);
+ *        }
+ *      }
+ *    }
+ *  }
+ *
+ *  cout << ans << endl;
+ *
+ */
+  return 0;
+}
+
