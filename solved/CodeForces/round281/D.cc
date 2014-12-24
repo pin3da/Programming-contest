c3ff0b8ede7083dbefefd9c99f3e55405edac22d
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2014-12-23 20:01:47 -0500

    Codeforces round 281 (D)

---
+++ solved/CodeForces/round281/D.cc
@@ -0,0 +1,12 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define ___ ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define D(x) cout << #x " = " << (x) << endl
+#define endl '\n'
+
+int main() { ___
+  int n;
+  cin >> n;
+  cout << ((n & 1) ? "black" : "white\n1 2" ) << endl;
+  return 0;
+}
