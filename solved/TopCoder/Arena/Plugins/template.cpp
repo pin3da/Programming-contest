6d5b186c7757c7a4a0d4cff0963f930e3561f4f0
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-02-09 22:42:48 -0500

    Moj instructions and configurations

---
+++ solved/TopCoder/Arena/Plugins/template.cpp
@@ -0,0 +1,18 @@
+// Paste me into the FileEdit configuration dialog
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class $CLASSNAME$ {
+public:
+   $RC$ $METHODNAME$( $METHODPARMS$ ) {
+
+   }
+};
+
+$BEGINCUT$
+$TESTCODE$
+
+$DEFAULTMAIN$
+$ENDCUT$
---
+++ solved/TopCoder/Arena/Plugins/template.java
@@ -0,0 +1,16 @@
+// Paste me into the FileEdit configuration dialog
+
+public class $CLASSNAME$ {
+   public $RC$ $METHODNAME$($METHODPARMS$) {
+		
+   }
+
+
+$BEGINCUT$
+   $DEFAULTMAIN$
+$ENDCUT$
+}
+
+$BEGINCUT$
+$TESTCODE$
+$ENDCUT$
