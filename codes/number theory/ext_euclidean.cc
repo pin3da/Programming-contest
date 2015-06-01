6e9215206a993bf66cc2ac42a9aeeea66195296d
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-05-31 23:15:17 -0500

    Added Chinese remainder theorem

---
+++ codes/number theory/ext_euclidean.cc
@@ -0,0 +1,9 @@
+void ext_euclid(long long a, long long b, long long &x, long long &y, long long &g) {
+  x = 0, y = 1, g = b;
+  long long m, n, q, r;
+  for (long long u = 1, v = 0; a != 0; g = a, a = r) {
+    q = g / a, r = g % a;
+    m = x - u * q, n = y - v * q;
+    x = u, y = v, u = m, v = n;
+  }
+}
---
+++ codes/number theory/mod_inv.cc
@@ -0,0 +1,7 @@
+long long mod_inv(long long n, long long m) {
+  long long x, y, gcd;
+  ext_euclid(n, m, x, y, gcd);
+  if (gcd != 1)
+    return 0;
+  return (x + m) % m;
+}
