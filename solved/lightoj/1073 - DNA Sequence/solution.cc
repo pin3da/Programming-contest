9efac71833cc08fc24a9b3d12b916a518d2d5112
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-06-18 23:28:33 -0500

    LightOJ, 1073 - DNA Sequence

--- solved/lightoj/1073 - DNA Sequence/solution.cc
+++ solved/lightoj/1073 - DNA Sequence/solution.cc
@@ -26,7 +26,7 @@ int inter(vector<string> &words, int a, int b) {
       return ov[a][b] = words[b].size() - overlap;
     }
   }
-  return ov[a][b] = 0;
+  return ov[a][b] = words[b].size();
 }
 
 
@@ -35,45 +35,67 @@ int op[111][(1 << 15) + 10];
 
 int go(vector<string> &words, int last, int seen) {
   if (seen == target)
-    return 0;
+    return dp[last + 1][seen] = 0;
 
-  if (dp[last + 1][seen] != 1)
+   if (dp[last + 1][seen] != -1)
     return dp[last + 1][seen];
 
   int ans = inf;
-
   for (int i = 0; i < words.size(); ++i) if (!((seen >> i) & 1)) {
-    int opt =  go(words, i, seen | (1 << i)) + inter(words, last, i);
-    // ans = min(ans, go(words, i, seen | (1 << i)) + inter(words, last, i));
-    if (opt < ans) {
-      ans = opt;
-      op[last + 1][seen] = i;
-    }
+    ans = min(ans, go(words, i, seen | (1 << i)) + inter(words, last, i));
   }
   return dp[last + 1][seen] = ans;
+}
+
+string best;
+void find_best(vector<string> &w, int last, int seen, string cur) {
+  if (cur > best)
+    return;
+
+  if (seen == target) {
+    if (cur < best)
+      best = cur;
+   return;
+  }
+
+  for (int i = 0; i < w.size(); ++i) if (!((seen >> i) & 1)) {
+    int need = inter(w, last, i);
+    if (dp[last + 1][seen] == (dp[i + 1][seen | (1 << i)] + need)) {
+      int index = w[i].size() - need;
+      find_best(w, i, seen | (1 << i), cur + w[i].substr(index, need));
+    }
+  }
 }
 
 void solve() {
   int n;
   cin >> n;
-  target = (1 << n) - 1;
   vector<string> words(n);
   for (int i = 0; i < n; ++i) {
     cin >> words[i];
   }
 
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+      if(i != j) if (words[i].find(words[j]) != string::npos)
+        words[j] = "";
+
+
+  vector<string> cleaned;
+  for (int i = 0; i < n; ++i)
+    if (words[i] != "")
+      cleaned.push_back(words[i]);
+
   memset(ov, -1, sizeof ov);
   memset(dp, -1, sizeof dp);
   memset(op, -1, sizeof op);
 
-  cout << go(words, -1, 0) << endl;
-  /*int state = 0, seen = 0;
-  while (op[state][seen] != -1) {
-    printf("%c", alphabet[op[state][seen]]);
-    state = findNextState(state, alphabet[op[state][seen]]);
-    seen = seen | out[state];
-  }
-  puts("");*/
+  target = (1 << cleaned.size()) - 1;
+  go(cleaned, -1, 0);
+  best = "Z";
+  find_best(cleaned, -1, 0, "");
+  cout << best << endl;
+
 }
 
 int main() {
