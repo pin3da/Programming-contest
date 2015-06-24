f8ba25fb72e51d22345c588edb2d684f1bfab333
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-06-24 07:45:30 -0500

    Sparse table - RMQ

---
+++ codes/data structures/sparse_table.cc
@@ -0,0 +1,28 @@
+// RMQ.
+const int MN = 100000 + 10; // Max number of elements
+const int ML = 18; // ceil(log2(MN));
+
+struct st {
+  int data[MN];
+  int M[MN][ML];
+  int n;
+
+  void read(int _n) {
+    n = _n;
+    for (int i = 0; i < n; ++i)
+      cin >> data[i];
+  }
+
+  void build() {
+    for (int i = 0; i < n; ++i)
+      M[i][0] = data[i];
+    for (int j = 1, p = 2, q = 1; p <= n; ++j, p <<= 1, q <<= 1)
+      for (int i = 0; i + p - 1 < n; ++i)
+        M[i][j] = max(M[i][j - 1], M[i + q][j - 1]);
+  }
+  int query(int b, int e) {
+    int k = log2(e - b + 1);
+    return max(M[b][k], M[e + 1 - (1<<k)][k]);
+  }
+};
+
