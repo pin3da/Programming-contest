9f3b7e4a2d8543d4927589bf3f1acea6841080e2
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-08-30 18:36:02 -0500

    UVa, 11090 - Going in Cycle!!

---
+++ solved/UVa/11090 - Going in Cycle!!/solution.cc
@@ -0,0 +1,84 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+const int MN = 1000;
+struct edge{
+  int v;
+  long long w;
+  edge(){} edge(int v, int w) : v(v), w(w) {}
+};
+
+long long d[MN][MN];
+int karp(vector<vector<edge> > g) {
+  int n = g.size();
+
+  g.resize(n + 1); // this is important
+
+  for (int i = 0; i < n; ++i)
+    if (!g[i].empty())
+      g[n].push_back(edge(i,0));
+  ++n;
+
+  for(int i = 0;i<n;++i)
+    fill(d[i],d[i]+(n+1),INT_MAX);
+
+  d[n - 1][0] = 0;
+
+  for (int k = 1; k <= n; ++k) for (int u = 0; u < n; ++u) {
+    if (d[u][k - 1] == INT_MAX) continue;
+    for (int i = g[u].size() - 1; i >= 0; --i)
+      d[g[u][i].v][k] = min(d[g[u][i].v][k], d[u][k - 1] + g[u][i].w);
+  }
+
+  bool flag = true;
+
+  for (int i = 0; i < n && flag; ++i)
+    if (d[i][n] != INT_MAX)
+      flag = false;
+
+  if (flag) {
+    return true;
+  }
+
+  double ans = 1e15;
+
+  for (int u = 0; u + 1 < n; ++u) {
+    if (d[u][n] == INT_MAX) continue;
+    double W = -1e15;
+
+    for (int k = 0; k < n; ++k)
+      if (d[u][k] != INT_MAX)
+        W = max(W, (double)(d[u][n] - d[u][k]) / (n - k));
+
+    ans = min(ans, W);
+  }
+
+  // printf("%.2lf\n", ans);
+  cout << fixed << setprecision(2) << ans << endl;
+
+  return false;
+}
+
+void solve() {
+  int n, m; cin >> n >> m;
+  vector<vector<edge> > g(n);
+  int a, b, c;
+  for (int i = 0; i < m; ++i) {
+    cin >> a >> b >> c;
+    a--;b--;
+    g[a].push_back(edge(b, c));
+  }
+
+  if (karp(g))
+    cout << "No cycle found." << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  int tc; cin >> tc;
+  for (int i = 0; i < tc; ++i) {
+    cout << "Case #" << (i + 1) << ": ";
+    solve();
+  }
+  return 0;
+}
