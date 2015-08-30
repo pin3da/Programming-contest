74a1b6e2eceee9ba7ada02abfc84a9b53d9dc2c0
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-08-30 14:29:18 -0500

    modified notebook

--- codes/Number theory/highest exponent factorial.cc
+++ codes/Number theory/highest exponent factorial.cc
@@ -1,8 +1,3 @@
-/**
- * Returns the highest exponent of a prime p dividing n!.
- * reference: http://math.stackexchange.com/questions/141196/highest-power-of-a-prime-p-dividing-n
- * */
-
 int highest_exponent(int p, const int &n){
   int ans = 0;
   int t = p;
