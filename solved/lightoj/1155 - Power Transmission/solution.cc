b447b42fe27168b9d12789b12c7c1142c6f907e2
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-09-02 12:16:55 -0500

    Lightoj, 1155 - Power Transmission

---
+++ solved/lightoj/1155 - Power Transmission/solution.cc
@@ -0,0 +1,108 @@
+using namespace std;
+#include <bits/stdc++.h>
+
+const int INF = 2000000000;
+
+struct Edge {
+  int from, to, cap, flow, index;
+  Edge(int from, int to, int cap, int flow, int index) :
+    from(from), to(to), cap(cap), flow(flow), index(index) {}
+};
+
+struct Dinic {
+  int N;
+  vector<vector<Edge> > G;
+  vector<Edge *> dad;
+  vector<int> Q;
+
+  Dinic(int N) : N(N), G(N), dad(N), Q(N) {}
+
+  void AddEdge(int from, int to, int cap) {
+    G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
+    if (from == to) G[from].back().index++;
+    G[to].push_back(Edge(to, from, 0, 0, G[from].size() - 1));
+  }
+
+  long long BlockingFlow(int s, int t) {
+    fill(dad.begin(), dad.end(), (Edge *) NULL);
+    dad[s] = &G[0][0] - 1;
+
+    int head = 0, tail = 0;
+    Q[tail++] = s;
+    while (head < tail) {
+      int x = Q[head++];
+      for (int i = 0; i < G[x].size(); i++) {
+        Edge &e = G[x][i];
+        if (!dad[e.to] && e.cap - e.flow > 0) {
+          dad[e.to] = &G[x][i];
+          Q[tail++] = e.to;
+        }
+      }
+    }
+    if (!dad[t]) return 0;
+
+    long long totflow = 0;
+    for (int i = 0; i < G[t].size(); i++) {
+      Edge *start = &G[G[t][i].to][G[t][i].index];
+      int amt = INF;
+      for (Edge *e = start; amt && e != dad[s]; e = dad[e->from]) {
+        if (!e) { amt = 0; break; }
+        amt = min(amt, e->cap - e->flow);
+      }
+      if (amt == 0) continue;
+      for (Edge *e = start; amt && e != dad[s]; e = dad[e->from]) {
+        e->flow += amt;
+        G[e->to][e->index].flow -= amt;
+      }
+      totflow += amt;
+    }
+    return totflow;
+  }
+
+  long long GetMaxFlow(int s, int t) {
+    long long totflow = 0;
+    while (long long flow = BlockingFlow(s, t))
+      totflow += flow;
+    return totflow;
+  }
+};
+
+
+void solve() {
+  int n, t, m;
+  cin >> n;
+  int source = n, sink = n + 1, next = n + 2;
+  Dinic g(2 * n + 4);
+  for (int i = 0; i < n; ++i) {
+    cin >> t;
+    g.AddEdge(i, i + next, t);
+  }
+  cin >> m;
+  int u, v, c;
+  for (int i = 0; i < m; ++i) {
+    cin >> u >> v >> c;
+    u--;v--;
+    g.AddEdge(next + u, v, c);
+  }
+  cin >> u >> v;
+  for (int i = 0; i < u; ++i) {
+    cin >> t;
+    g.AddEdge(source, t - 1, INF);
+  }
+
+  for (int i = 0; i < v; ++i) {
+    cin >> t;
+    g.AddEdge(next + t - 1, sink, INF);
+  }
+  printf("%lld\n", g.GetMaxFlow(source, sink));
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  int tc; cin >> tc;
+  for (int i = 0; i < tc; ++i) {
+    printf("Case %d: ", i + 1);
+    solve();
+  }
+  return 0;
+}
