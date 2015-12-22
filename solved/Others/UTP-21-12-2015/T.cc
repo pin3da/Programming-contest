3356f99cacfcd4997791f11769bc19b2a331c919
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-12-21 23:31:33 -0500

    ICPC Training

---
+++ solved/Others/UTP-21-12-2015/T.cc
@@ -0,0 +1,11 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define D(x) cout << #x " = " << (x) << endl
+#define endl '\n'
+
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+  return 0;
+}
