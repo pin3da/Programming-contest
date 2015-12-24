a25e0e2cb4e079f6731d3114c2575175927d2868
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2015-12-23 23:03:23 -0500

    ICPC Training

---
+++ solved/Others/UTP-23-12-2015/T.cc
@@ -0,0 +1,14 @@
+using namespace std;
+#include <bits/stdc++.h>
+#define D(x) cout << #x " = " << (x) << endl
+
+void solve(int n) {
+
+}
+
+int main() {
+  int n;
+  while(cin >> n)
+    solve(n);
+  return 0;
+}
