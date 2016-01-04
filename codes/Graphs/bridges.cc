f12651d444ab1ff97d567ec481dc05dd7fde5b17
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-01-04 12:05:18 -0500

    Added bridges code

---
+++ codes/Graphs/bridges.cc
@@ -0,0 +1,59 @@
+struct edge{
+  int to, id;
+  edge(int a, int b) : to(a), id(b) {}
+};
+
+struct graph {
+  vector<vector<edge> > g;
+  vector<int> vi, low, d, pi, is_b;
+
+  int ticks, edges;
+
+  graph(int n, int m) {
+    g.assign(n, vector<edge>());
+    is_b.assign(m, 0);
+    vi.resize(n);
+    low.resize(n);
+    d.resize(n);
+    pi.resize(n);
+    edges = 0;
+  }
+
+  void add_edge(int u, int v) {
+    g[u].push_back(edge(v, edges));
+    g[v].push_back(edge(u, edges));
+    edges++;
+  }
+
+  void dfs(int u) {
+    vi[u] = true;
+    d[u] = low[u] = ticks++;
+    for (int i = 0; i < g[u].size(); ++i) {
+      int v = g[u][i].to;
+      if (v == pi[u]) continue;
+      if (!vi[v]) {
+        pi[v] = u;
+        dfs(v);
+        if (d[u] < low[v])
+          is_b[g[u][i].id] = true;
+
+
+        low[u] = min(low[u], low[v]);
+      } else {
+        low[u] = min(low[u], d[v]);
+      }
+    }
+  }
+
+  void comp_bridges() {
+    fill(pi.begin(), pi.end(), -1);
+    fill(vi.begin(), vi.end(), 0);
+    fill(low.begin(), low.end(), 0);
+    fill(d.begin(), d.end(), 0);
+    ticks = 0;
+    for (int i = 0; i < g.size(); ++i)
+      if (!vi[i]) dfs(i);
+  }
+};
+
+
