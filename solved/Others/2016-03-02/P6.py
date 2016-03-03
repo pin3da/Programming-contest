cd9832937193caf33aff2e2ee5713f83c5af89a7
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-03-02 22:51:16 -0500

    ICPC training

---
+++ solved/Others/2016-03-02/P6.py
@@ -0,0 +1,11 @@
+tc = int(raw_input())
+
+while (tc > 0):
+    line = int(raw_input())
+    line = bin(line)
+    tc = tc - 1
+    ans = 0
+    for i in line:
+        if (i == '1'):
+            ans = ans + 1
+    print ans
