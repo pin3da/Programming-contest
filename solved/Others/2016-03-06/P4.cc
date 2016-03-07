7381a6c696cee99373f33312c942511f981a0d51
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-03-06 22:26:05 -0500

    icpc training

---
+++ solved/Others/2016-03-06/P4.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define D(x) cout << #x " = " << (x) << endl
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+  return 0;
+}
