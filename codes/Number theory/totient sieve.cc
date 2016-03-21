8f250f80385a20c7d5fa1f6091b5b153954db07d
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-03-21 18:59:10 -0500

    Update totient sieve.cc

--- codes/Number theory/totient sieve.cc
+++ codes/Number theory/totient sieve.cc
@@ -1,9 +1,7 @@
-for (int i = 0; i < MP; ++i) phi[i] = i;
+  for (int i = 1; i < MN; i++)
+    phi[i] = i;
 
-for (int i = 0; primes[i] <= 5000000; ++i) {
-    phi[primes[i]] = primes[i] - 1;
-    for (i64 j = 2 * primes[i]; j <= 5000000; j += primes[i]) {
-        phi[j] = phi[j] * (primes[i]-1);
-        phi[j] = phi[j] / primes[i];
-    }
-}
+  for (int i = 1; i < MN; i++)
+    if (!sieve[i]) // is prime
+      for (int j = i; j < MN; j += i)
+        phi[j] -= phi[j] / i;
