39c3bd690740cad0b08900e30b545388e406d0c4
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-03-30 14:09:13 -0500

    added Codeforces 346

---
+++ solved/Codeforces/Codeforces Round #346 (Div. 2)/A.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define D(x) cout << #x " = " << (x) << endl
+#define endl '\n'
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  int n, a, b;
+  while (cin >> n >> a >> b) {
+    a--;
+    cout << (((a + b) % n + n) % n) + 1 << endl;
+  }
+
+  return 0;
+}
