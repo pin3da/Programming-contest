2a1641d207b87a0cca7c97ee34158a992567eb33
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-04-15 22:37:42 -0500

    added CodeJam round 1A

---
+++ solved/CodeJam/2016/Round 1A/T.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define D(x) cout << #x " = " << (x) << endl
+#define endl '\n'
+using namespace std;
+
+void solve() {
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  int tc; cin >> tc;
+  int c = 1;
+  while (tc--) {
+    cout << "Case #"  << (c++) << ": ";
+    solve();
+  }
+  return 0;
+}
