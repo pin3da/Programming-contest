67a8ca1ffde2ff3b36ceb0642f66a96270f69af4
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-05-14 19:41:03 -0500

    Fixed Codeforces problems

--- solved/Codeforces/Codeforces Round #352 (Div. 2)/C.cc
+++ solved/Codeforces/Codeforces Round #352 (Div. 2)/C.cc
@@ -10,64 +10,44 @@ struct PT {
   double x, y;
   PT() {}
   PT(double x, double y) : x(x), y(y) {}
-  PT(const PT &p) : x(p.x), y(p.y)    {}
-  PT operator + (const PT &p)  const { return PT(x+p.x, y+p.y); }
-  PT operator - (const PT &p)  const { return PT(x-p.x, y-p.y); }
-  PT operator * (double c)     const { return PT(x*c,   y*c  ); }
-  PT operator / (double c)     const { return PT(x/c,   y/c  ); }
 };
 
-double dot(PT p, PT q)     { return p.x*q.x+p.y*q.y; }
-double dist2(PT p, PT q)   { return dot(p-q,p-q); }
-double cross(PT p, PT q)   { return p.x*q.y-p.y*q.x; }
-double dist(PT p, PT q)    { return sqrt(dist2(p, q));}
-ostream &operator<<(ostream &os, const PT &p) {
-  os << "(" << p.x << "," << p.y << ")";
-}
-
-double find_best(vector<double> &d, vector<PT> &p, PT a, PT b, PT r, double total) {
-  int id_a = -1;
-  double best = 1e100;
-  for (int i = 0; i < p.size(); ++i) {
-    double cur = total - d[i] + dist(p[i], a);
-    if (cur < best) {
-      best = cur;
-      id_a = i;
-    }
-  }
-  double ans = 1e100;
-  for (int i = 0; i < p.size(); ++i) {
-    if (i == id_a) continue;
-    double cur = best - d[i] + dist(p[i], b);
-    ans = min(ans, cur);
-  }
-  return ans;
+double dist(PT p, PT q) {
+  double x = p.x - q.x,
+         y = p.y - q.y;
+  return sqrt(x * x + y * y);
 }
 
 int main() {
   ios_base::sync_with_stdio(false);cin.tie(NULL);
   PT a, b, r;
-  cout << fixed << setprecision(10);
+  cout << fixed << setprecision(14);
   while (cin >> a.x >> a.y >> b.x >> b.y >> r.x >> r.y) {
     int n; cin >> n;
     vector<PT> p(n);
     vector<double> d(n);
+    vector<pair<double, int>> ba(n), bb(n);
     double total = 0;
     int j = 0;
     for (auto &i : p) {
       cin >> i.x >> i.y;
       d[j] = dist(i, r);
+      ba[j].first = -d[j] + dist(i, a);
+      bb[j].first = -d[j] + dist(i, b);
+      ba[j].second = j;
+      bb[j].second = j;
       total += 2.0 * d[j];
       j++;
     }
 
-    double best = total + total;
-    best = min(best, find_best(d, p, a, b, r, total));
-    best = min(best, find_best(d, p, b, a, r, total));
-    for (int i = 0; i < n; ++i) {
-      best = min(best, total - d[i] + dist(a, r));
-      best = min(best, total - d[i] + dist(b, r));
-    }
+    sort(ba.begin(), ba.end());
+    sort(bb.begin(), bb.end());
+    double best = total;
+    if (ba[0].second != bb[0].second)
+      best += ba[0].first + bb[0].first;
+    else
+      best += min(ba[0].first + bb[1].first, ba[1].first + bb[0].first);
+    best = min(best, min(total + ba[0].first, total + bb[0].first));
     cout << best << endl;
   }
   return 0;
--- solved/Codeforces/Codeforces Round #352 (Div. 2)/D.cc
+++ solved/Codeforces/Codeforces Round #352 (Div. 2)/D.cc
@@ -8,47 +8,40 @@ int main() {
   long long n, k;
   while (cin >> n >> k) {
     vector<long long> a(n);
-    for (auto &i : a) cin >> i;
-    long long total = accumulate(a.begin(), a.end(), 0),
-         extra = total % n,
-         real  = (total - extra) / n,
-         diff  = 0;
-
-    for (int i = 0; i < n; ++i) {
-      diff += abs(a[i] - real);
+    long long total = 0;
+    for (auto &i : a) {
+      cin >> i;
+      total += i;
     }
-
-    long long happy = (diff >> 1) + extra;
-
-    if (k >= happy) {
-      cout << (extra != 0) << endl;
-    } else {
-      map<int, int> s;
+    sort(a.begin(), a.end());
+    long long lo = 0, hi = (total / n);
+    while (lo < hi) {
+      long long mid = (lo + hi + 1) >> 1;
+      long long need = 0;
       for (int i = 0; i < n; ++i) {
-        s[a[i]]++;
+        need += max(0LL, mid - a[i]);
       }
-
-      while (k--) {
-        int a = (s.begin())-> first;
-        auto e = s.end();
-        e--;
-        int b = e-> first;
-        if (a < b) {
-          s[a]--;
-          s[b]--;
-          if (s[a] == 0) s.erase(a);
-          if (s[b] == 0) s.erase(b);
-          s[++a]++;
-          s[--b]++;
-        }
-      }
-      int a = (s.begin())-> first;
-      auto e = s.end();
-      e--;
-      int b = e-> first;
-      cout << b - a << endl;
-
+      if (need > k)
+        hi = mid - 1;
+      else
+        lo = mid;
     }
+
+    long long tmp1 = lo;
+
+    lo = (total + n - 1) / n, hi = total;
+    while (lo < hi) {
+      long long mid = (lo + hi) >> 1;
+      long long need = 0;
+      for (int i = 0; i < n; ++i) {
+        need += max(0LL, a[i] - mid);
+      }
+      if (need > k)
+        lo = mid + 1;
+      else
+        hi = mid;
+    }
+    cout << llabs(lo - tmp1) << endl;
   }
   return 0;
 }
