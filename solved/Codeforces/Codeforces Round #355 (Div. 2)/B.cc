cef08254ce942cacf1406e7fc18f9d7933872ab0
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-06-01 14:02:43 -0500

    added codeforces round 355

---
+++ solved/Codeforces/Codeforces Round #355 (Div. 2)/B.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define D(x) cout << #x " = " << (x) << endl
+#define endl '\n'
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  long long n, h, k;
+  while (cin >> n >> h >> k) {
+    vector<long long> a(n);
+    for (auto &i : a) cin >> i;
+
+    long long rem = 0;
+    long long ans = 0;
+    for (auto &i : a) {
+      if (rem + i > h) {
+        ans += (rem + k - 1) / k;
+        ans += i / k;
+        rem = i % k;
+      } else {
+        long long t = rem + i;
+        ans += t / k;
+        rem = t % k;
+      }
+    }
+    ans += (rem + k - 1) / k;
+    cout << ans << endl;
+  }
+  return 0;
+}
