7f5cfa20edb4cda6f229d41dd7dd4fdf1565a3ca
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2016-07-02 18:31:55 -0500

    added suffix automaton

---
+++ codes/Strings/suffix automaton.cc
@@ -0,0 +1,66 @@
+/*
+ * Suffix automaton:
+ * This implementation was extended to maintain (online) the
+ * number of different substrings. This is equivalent to compute
+ * the number of paths from the initial state to all the other
+ * states.
+ *
+ * The overall complexity is O(n)
+ * can be tested here: https://www.urionlinejudge.com.br/judge/en/problems/view/1530
+ * */
+
+struct state {
+  int len, link;
+  long long num_paths;
+  unordered_map<int, int> next;
+};
+
+const int MN = 200011;
+state sa[MN << 1];
+int sz, last;
+long long tot_paths;
+
+void sa_init() {
+  sz = 1;
+  last = 0;
+  sa[0].len = 0;
+  sa[0].link = -1;
+  sa[0].next.clear();
+  sa[0].num_paths = 1;
+  tot_paths = 0;
+}
+
+void sa_extend(int c) {
+  int cur = sz++;
+  sa[cur].len = sa[last].len + 1;
+  sa[cur].next.clear();
+  sa[cur].num_paths = 0;
+  int p;
+  for (p = last; p != -1 && !sa[p].next.count(c); p = sa[p].link) {
+    sa[p].next[c] = cur;
+    sa[cur].num_paths += sa[p].num_paths;
+    tot_paths += sa[p].num_paths;
+  }
+
+  if (p == -1) {
+    sa[cur].link = 0;
+  } else {
+    int q = sa[p].next[c];
+    if (sa[p].len + 1 == sa[q].len) {
+      sa[cur].link = q;
+    } else {
+      int clone = sz++;
+      sa[clone].len = sa[p].len + 1;
+      sa[clone].next = sa[q].next;
+      sa[clone].num_paths = 0;
+      sa[clone].link = sa[q].link;
+      for (; p!= -1 && sa[p].next[c] == q; p = sa[p].link) {
+        sa[p].next[c] = clone;
+        sa[q].num_paths -= sa[p].num_paths;
+        sa[clone].num_paths += sa[p].num_paths;
+      }
+      sa[q].link = sa[cur].link = clone;
+    }
+  }
+  last = cur;
+}
---
+++ solved/URI/1530 How Many Substrings?/solution.cc
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define debug(x) cout << #x " = " << (x) << endl
+#define endl '\n'
+
+using namespace std;
+
+struct state {
+  int len, link;
+  long long num_paths;
+  unordered_map<int, int> next;
+};
+
+const int MN = 200011;
+state sa[MN << 1];
+int sz, last;
+long long tot_paths;
+
+void sa_init() {
+  sz = 1;
+  last = 0;
+  sa[0].len = 0;
+  sa[0].link = -1;
+  sa[0].next.clear();
+  sa[0].num_paths = 1;
+  tot_paths = 0;
+}
+
+void debug_next(int a, int b) {
+  cout << "from " << a << " to " << b << endl;
+}
+
+void debug_num_paths() {
+  long long total = 0;
+  for (int i = 1; i < sz; ++i) {
+    cout << sa[i].num_paths << " ";
+    total += sa[i].num_paths;
+  }
+  cout << ": " << total << endl;
+}
+
+void sa_extend(int c) {
+  int cur = sz++;
+  sa[cur].len = sa[last].len + 1;
+  sa[cur].next.clear();
+  sa[cur].num_paths = 0;
+  int p;
+  for (p = last; p != -1 && !sa[p].next.count(c); p = sa[p].link) {
+    sa[p].next[c] = cur;
+    sa[cur].num_paths += sa[p].num_paths;
+    tot_paths += sa[p].num_paths;
+  }
+
+  if (p == -1) {
+    sa[cur].link = 0;
+  } else {
+    int q = sa[p].next[c];
+    if (sa[p].len + 1 == sa[q].len) {
+      sa[cur].link = q;
+    } else {
+      int clone = sz++;
+      sa[clone].len = sa[p].len + 1;
+      sa[clone].next = sa[q].next;
+      sa[clone].num_paths = 0;
+      sa[clone].link = sa[q].link;
+      for (; p!= -1 && sa[p].next[c] == q; p = sa[p].link) {
+        sa[p].next[c] = clone;
+        sa[q].num_paths -= sa[p].num_paths;
+        sa[clone].num_paths += sa[p].num_paths;
+      }
+      sa[q].link = sa[cur].link = clone;
+    }
+  }
+  last = cur;
+}
+
+
+int main() {
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  string line;
+  while (cin >> line) {
+    sa_init();
+    for (auto &i : line) {
+      if (i == '?') {
+        cout << tot_paths << endl;
+        // debug_num_paths();
+      } else
+        sa_extend(i);
+    }
+  }
+  return 0;
+}
