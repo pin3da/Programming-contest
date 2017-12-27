4e48c6b4d2109f4b77f24158c2130ff0496e56f8
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2017-12-27 17:10:02 -0500

    wrap seg_tree into struct to copy-paste easily

--- codes/Data structures/segment tree.cc
+++ codes/Data structures/segment tree.cc
@@ -2,27 +2,36 @@
  * Taken from: http://codeforces.com/blog/entry/18051
  * */
 
-const int N = 1e5;  // limit for array size
-int n;  // array size
-int t[2 * N];
+const int MN = 1e5;  // limit for array size
 
-void build() {  // build the tree
-  for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
-}
+struct seg_tree {
+  int n;  // array size
+  int t[2 * MN];
 
-// Single modification, range query.
-void modify(int p, int value) {  // set value at position p
-  for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
-}
+  seg_tree(int _n) : n(_n) {}
 
-int query(int l, int r) {  // sum on interval [l, r)
-  int res = 0;
-  for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-    if (l&1) res += t[l++];
-    if (r&1) res += t[--r];
+  void clear() {
+    memset(t, 0, sizeof t);
   }
-  return res;
-}
+
+  void build() {  // build the tree
+    for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
+  }
+
+  // Single modification, range query.
+  void modify(int p, int value) {  // set value at position p
+    for (t[p += n] = value; p > 1; p >>= 1) t[p>>1] = t[p] + t[p^1];
+  }
+
+  int query(int l, int r) {  // sum on interval [l, r)
+    int res = 0;
+    for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+      if (l&1) res += t[l++];
+      if (r&1) res += t[--r];
+    }
+    return res;
+  }
+};
 
 // Range modification, single query.
 
