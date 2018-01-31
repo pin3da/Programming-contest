0d79a56cf7ce5f888e835d22b52cb4c618fdcff1
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2018-01-31 12:45:58 -0500

    add segment tree for intervals

--- codes/Data structures/segment tree.cc
+++ codes/Data structures/segment tree.cc
@@ -79,4 +79,68 @@ S query(int l, int r) {
   return combine(resl, resr);
 }
 
-// To be continued ...
+
+/**
+ * segment tree for intervals
+ *
+ * */
+
+const int MN = 100000 + 100;
+struct seg_tree {
+  int val[MN * 4 + 4];
+  int pending[MN * 4 + 4];
+
+  seg_tree() {
+    memset(val, -1, sizeof val);
+    memset(pending, -1, sizeof pending);
+  }
+
+  void propagate(int node, int b, int e) {
+    if (pending[node] != -1) {
+      val[node] = pending[node];
+      if (b < e) {
+        pending[node << 1] = pending[node];
+        pending[node << 1 | 1] = pending[node];
+      }
+      pending[node] = -1;
+    }
+  }
+
+  void set(int node, int b, int e, int from, int to, int v) {
+    if (b > to || e < from) return;
+
+    if (b >= from && e <= to) {
+      pending[node] = v;
+      propagate(node, b, e);
+      return;
+    }
+
+    int mid = (b + e) >> 1;
+
+    set(node << 1, b, mid, from, to, v);
+    set(node << 1 | 1, mid + 1, e, from, to, v);
+  }
+
+
+
+  int query(int node, int b, int e, int pos) {
+    propagate(node, b, e);
+
+    if (b == e && b == pos) {
+      return val[node];
+    }
+
+    int mid = (b + e) >> 1;
+    if (pos <= mid)
+      return query(node << 1, b, mid, pos);
+    return query(node << 1 | 1, mid + 1, e, pos);
+  }
+
+  void set(int from, int to, int v) {
+    return set(1, 0, MN - 1, from, to, v);
+  }
+
+  int query(int pos) {
+    return query(1, 0, MN - 1, pos);
+  }
+};
