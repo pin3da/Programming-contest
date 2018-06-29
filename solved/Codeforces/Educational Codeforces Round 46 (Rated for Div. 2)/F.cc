858f15de24653b9a2312ff8eaafa61d27df38353
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2018-06-29 14:37:38 -0500

    add problem about sqrt decomposition

--- solved/Codeforces/Educational Codeforces Round 46 (Rated for Div. 2)/F.cc
+++ solved/Codeforces/Educational Codeforces Round 46 (Rated for Div. 2)/F.cc
@@ -1,4 +1,3 @@
-// MLE test 69 ):
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -14,45 +13,52 @@ void debug_out(H h, T... t) {
 const int MN = 5 * 100000 + 1;
 const int SN = 708;
 
-struct query {
+struct Query {
   int a, b, id;
-  query() {}
-  query(int x, int y, int i) : a(x), b(y), id(i) {}
+  Query() {}
+  Query(int x, int y, int i) : a(x), b(y), id(i) {}
 
-  bool operator<(const query &o) const { return b < o.b; }
+  bool operator<(const Query &o) const {
+    if (a / SN != o.a / SN)
+      return a < o.a;
+    return a / SN & 1 ? b < o.b : b > o.b;
+  }
 };
 
-vector<query> s[SN];
-int ans[MN];
 
 struct DS {
   vector<int> frec, ans;
+  vector<bool> seen;
 
-  DS () : frec(MN) {}
+  DS () : frec(MN), seen(MN) {}
 
-  void clear() {
-    fill(frec.begin(), frec.end(), 0);
-    ans.clear();
-  }
-
-  void insert(int x) {
+  void Insert(int x) {
     frec[x]++;
-    int f = frec[x];
-    if (f == 1) ans.push_back(x);
+    if (frec[x] == 1 && !seen[x]) {
+      ans.push_back(x);
+      seen[x] = true;
+    }
   }
 
-  void erase(int x) {
+  void Erase(int x) {
     frec[x]--;
-    int f = frec[x];
-    if (f == 1) ans.push_back(x);
+    if (frec[x] == 1 && !seen[x]) {
+      ans.push_back(x);
+      seen[x] = true;
+    }
   }
 
-  long long query() {
-    while (ans.size() && frec[ans.back()] != 1) ans.pop_back();
+  long long Query() {
+    while (ans.size() && frec[ans.back()] != 1) {
+      seen[ans.back()] = false;
+      ans.pop_back();
+    }
     return ans.size() ? ans.back() : 0;
   }
 };
 
+Query s[MN];
+int ans[MN];
 DS active;
 
 int main() {
@@ -74,30 +80,23 @@ int main() {
     cin >> b >> e;
     b--;
     e--;
-    s[b / SN].emplace_back(b, e, i);
+    s[i] = Query(b, e, i);
   }
 
-  for (int i = 0; i < SN; ++i) {
-    if (s[i].size()) sort(s[i].begin(), s[i].end());
-  }
+  sort(s, s + q);
 
-  for (int b = 0; b < SN; ++b) {
-    if (s[b].size() == 0) continue;
-    int i = s[b][0].a;
-    int j = s[b][0].a - 1;
+  int i = 0;
+  int j = -1;
+  for (int k = 0; k < (int)q; ++k) {
+    int L = s[k].a;
+    int R = s[k].b;
 
-    active.clear();
-    for (int k = 0; k < (int)s[b].size(); ++k) {
-      int L = s[b][k].a;
-      int R = s[b][k].b;
+    while (j < R) active.Insert(a[++j]);
+    while (j > R) active.Erase(a[j--]);
+    while (i < L) active.Erase(a[i++]);
+    while (i > L) active.Insert(a[--i]);
 
-      while (j < R) active.insert(a[++j]);
-      while (j > R) active.erase(a[j--]);
-      while (i < L) active.erase(a[i++]);
-      while (i > L) active.insert(a[--i]);
-
-      ans[s[b][k].id] = active.query();
-    }
+    ans[s[k].id] = active.Query();
   }
 
   for (int i = 0; i < q; ++i) {
