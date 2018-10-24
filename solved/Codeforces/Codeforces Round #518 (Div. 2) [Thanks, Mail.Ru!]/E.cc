d1ff96821bc3cf56a66fd7f78738c2a3247ef4e9
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2018-10-24 16:04:49 -0500

    add CF 518

--- solved/Codeforces/Codeforces Round #518 (Div. 2) [Thanks, Mail.Ru!]/E.cc
+++ solved/Codeforces/Codeforces Round #518 (Div. 2) [Thanks, Mail.Ru!]/E.cc
@@ -36,16 +36,21 @@ int main() {
   for (int i = 0; i < n; i++) {
     if (degree[i] == 1) {
       q.push_back(i);
-      seen[i] = true;
+      seen[i] = 1;
     }
   }
 
   int ok = true;
+  int cur = 1;
   while (!q.empty() && k > 0) {
     set<int> nxt;
     for (auto node : q) {
       for (auto to : g[node]) {
-        if (seen[to]) continue;
+        if (seen[to] && (seen[to] < seen[node])) continue;
+        if (seen[to] && (seen[to] == seen[node])) {
+          ok = false;
+          continue;
+        }
         nxt.insert(to);
         inc[to]++;
       }
@@ -57,13 +62,13 @@ int main() {
         ok = false;
         break;
       }
-      seen[it] = true;
+      seen[it] = cur + 1;
     }
     if (!ok) break;
     q.clear();
     for (auto it : nxt) q.push_back(it);
     k--;
-
+    cur++;
     if (k == 0) ok &= (q.size() == 1);
   }
 
