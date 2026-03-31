2a85dab1a5455d325d3f94f0719d1d30b3ff00ce
Manuel Pineda <manuel.felipe.pineda@gmail.com>
2026-03-31 10:30:10 +0200

    Add Codeforces round 1089 A.

---
+++ solved/Codeforces/Codeforces Round 1089 (Div. 2)/A.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+      cout << n - i << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
